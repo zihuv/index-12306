f40a1ef4e27ddc48fc8b023f51e9e83916786731
zihuv <zihuv@qq.com>
2023-09-13 14:55:09 +0800

    构建mybatis plus数据库存储模块

---
+++ frameworks/database/src/main/java/com/zihuv/database/base/BaseDO.java
@@ -0,0 +1,33 @@
+package com.zihuv.database.base;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * 数据持久层基础属性
+ */
+@Data
+public class BaseDO {
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标志
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
