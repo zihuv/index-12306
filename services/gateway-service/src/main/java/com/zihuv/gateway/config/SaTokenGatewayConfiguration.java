682b19fc9b08dd39137e1c36d806de9238c95b6d
zihuv <zihuv@qq.com>
2023-09-15 12:25:03 +0800

    feat: gateway 集成 knife4j

--- services/gateway-service/src/main/java/com/zihuv/gateway/config/SaTokenGatewayConfiguration.java
+++ services/gateway-service/src/main/java/com/zihuv/gateway/config/SaTokenGatewayConfiguration.java
@@ -23,7 +23,7 @@ public class SaTokenGatewayConfiguration {
                 // 拦截地址
                 .addInclude("/**")    /* 拦截全部path */
                 // 开放地址
-                .addExclude("/favicon.ico")
+                .addExclude("/favicon.ico","/doc.html")
                 // 鉴权方法：每次访问进入
                 .setAuth(obj -> {
                     // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
