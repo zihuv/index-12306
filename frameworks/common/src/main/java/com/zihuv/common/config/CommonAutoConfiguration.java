c5b345e20c18b4450a72a44e4676453ae9d20371
zihuv <zihuv@qq.com>
2023-09-16 16:44:23 +0800

    refactor: 修改 ObjectMapper Bean 的注测为主

--- frameworks/common/src/main/java/com/zihuv/common/config/CommonAutoConfiguration.java
+++ frameworks/common/src/main/java/com/zihuv/common/config/CommonAutoConfiguration.java
@@ -1,14 +1,11 @@
 package com.zihuv.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonAutoConfiguration {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 }
