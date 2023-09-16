d88b13967d99c3706ffdca018d1eab8c071c30fe
zihuv <zihuv@qq.com>
2023-09-16 18:45:55 +0800

    feat: 构建用户上下文模块

---
+++ frameworks/bizs/user/src/main/java/com/zihuv/index12306/frameworks/starter/user/core/UserContext.java
@@ -0,0 +1,9 @@
+package com.zihuv.index12306.frameworks.starter.user.core;
+
+/**
+ * 用户上下文
+ */
+public interface UserContext {
+
+    String getUsername();
+}
---
+++ frameworks/bizs/user/src/main/java/com/zihuv/index12306/frameworks/starter/user/core/UserInfoDTO.java
@@ -0,0 +1,31 @@
+package com.zihuv.index12306.frameworks.starter.user.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 用户 Token
+     */
+    private String token;
+}
