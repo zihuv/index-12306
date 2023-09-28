3f702bb81db65ae64dac9b1d047a64a2abf171e6
zihuv <zihuv@qq.com>
2023-09-28 15:31:51 +0800

    feat: 添加用户乘客中间表

---
+++ services/user-service/user-service-func/src/main/java/com/zihuv/userservice/mapper/UserPassengerMapper.java
@@ -0,0 +1,14 @@
+package com.zihuv.userservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zihuv.userservice.model.entity.UserPassenger;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserPassengerMapper extends BaseMapper<UserPassenger> {
+
+}
+
+
+
+
---
+++ services/user-service/user-service-func/src/main/java/com/zihuv/userservice/service/impl/UserPassengerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.zihuv.userservice.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zihuv.userservice.mapper.UserPassengerMapper;
+import com.zihuv.userservice.model.entity.UserPassenger;
+import com.zihuv.userservice.service.UserPassengerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPassengerServiceImpl extends ServiceImpl<UserPassengerMapper, UserPassenger> implements UserPassengerService {
+
+}
+
+
+
+
