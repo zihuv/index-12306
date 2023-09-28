0d0b6bb42f013fd52fee32e467ffdc3e466cdcd4
zihuv <zihuv@qq.com>
2023-09-28 15:31:51 +0800

    feat: 添加用户乘客中间表

---
+++ services/user-service/user-service-func/src/main/java/com/zihuv/userservice/service/UserPassengerService.java
@@ -0,0 +1,8 @@
+package com.zihuv.userservice.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zihuv.userservice.model.entity.UserPassenger;
+
+public interface UserPassengerService extends IService<UserPassenger> {
+
+}
