cf1faa6c19126a4bf2e408b326dcb0cfa7d54479
zihuv <zihuv@qq.com>
2023-09-28 15:35:23 +0800

    feat: 1.为所有 pom.xml 文件添加编码为 UTF-8，编译运行为 jdk17 2.为服务模块添加打包插件

---
+++ services/ticket-service/ticket-service-func/src/main/java/com/zihuv/ticketservice/TicketServiceApplication.java
@@ -0,0 +1,19 @@
+package com.zihuv.ticketservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@EnableDiscoveryClient
+@MapperScan("com.zihuv.*.mapper")
+@SpringBootApplication
+public class TicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApplication.class, args);
+    }
+
+}
