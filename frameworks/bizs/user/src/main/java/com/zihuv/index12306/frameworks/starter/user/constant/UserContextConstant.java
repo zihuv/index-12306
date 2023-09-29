8625b85b6460d554308f0bb3383da36e8a6dfce4
zihuv <zihuv@qq.com>
2023-09-29 22:21:52 +0800

    feat: 定义 UserContextConstant，便于在 SpEL 表达式中调用

---
+++ frameworks/bizs/user/src/main/java/com/zihuv/index12306/frameworks/starter/user/constant/UserContextConstant.java
@@ -0,0 +1,6 @@
+package com.zihuv.index12306.frameworks.starter.user.constant;
+
+public class UserContextConstant {
+
+    public static final String USER_CONTEXT = "T(com.zihuv.index12306.frameworks.starter.user.core.UserContext).getUsername()";
+}
