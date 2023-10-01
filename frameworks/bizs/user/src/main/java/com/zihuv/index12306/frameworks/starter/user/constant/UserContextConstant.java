c320ede2eda8049f2567b45406826b06a35e18b0
zihuv <zihuv@qq.com>
2023-10-01 16:25:04 +0800

    style: 修改 UserContextConstant 变量名

--- frameworks/bizs/user/src/main/java/com/zihuv/index12306/frameworks/starter/user/constant/UserContextConstant.java
+++ frameworks/bizs/user/src/main/java/com/zihuv/index12306/frameworks/starter/user/constant/UserContextConstant.java
@@ -2,5 +2,5 @@ package com.zihuv.index12306.frameworks.starter.user.constant;
 
 public class UserContextConstant {
 
-    public static final String USER_CONTEXT = "T(com.zihuv.index12306.frameworks.starter.user.core.UserContext).getUsername()";
+    public static final String USER_CONTEXT_SPEL = "T(com.zihuv.index12306.frameworks.starter.user.core.UserContext).getUsername()";
 }
