783489cdc099d5e8f0c1afbc7eaea21bf779d635
zihuv <zihuv@qq.com>
2023-10-04 09:06:26 +0800

    test: 添加 Ticket 测试类

---
+++ services/ticket-service/ticket-service-func/src/test/java/com/zihuv/ticketservice/TicketTest.java
@@ -0,0 +1,13 @@
+package com.zihuv.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TicketTest {
+
+    @Test
+    public void test01() {
+
+    }
+}
