c5d013d4c8dd687bbc4bbf95963c8e68429c12fd
zihuv <zihuv@qq.com>
2023-11-18 22:01:49 +0800

    feat: 构建 OrderFeign 的 feign 服务

--- services/order-service/order-service-feign/src/main/java/com/zihuv/orderservice/feign/OrderFeign.java
+++ services/order-service/order-service-feign/src/main/java/com/zihuv/orderservice/feign/OrderFeign.java
@@ -1,7 +1,14 @@
 package com.zihuv.orderservice.feign;
 
+import com.zihuv.convention.result.Result;
+import com.zihuv.orderservice.model.param.TicketOrderCreateParam;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "index12306-order-service")
 public interface OrderFeign {
+
+    @PostMapping("/api/order-service/order/create")
+    Result<?> createOrder(@RequestBody TicketOrderCreateParam requestParam);
 }
