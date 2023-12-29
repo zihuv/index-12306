917702f9168cb4fd363be9b4018afddfc8348fc4
zihuv <zihuv@qq.com>
2023-12-29 15:29:41 +0800

    feat: 添加本地消息表序列化方式

--- frameworks/mq/src/main/java/com/zihuv/mq/domain/LocalMessage.java
+++ frameworks/mq/src/main/java/com/zihuv/mq/domain/LocalMessage.java
@@ -1,7 +1,9 @@
 package com.zihuv.mq.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.zihuv.database.base.BaseDO;
 import lombok.Data;
 import org.springframework.messaging.Message;
@@ -15,6 +17,7 @@ public class LocalMessage extends BaseDO {
 
     private String destination;
 
+    @TableField(typeHandler = JacksonTypeHandler.class)
     private Message<?> messageBody;
 
     private Integer sendStatus = 0;
